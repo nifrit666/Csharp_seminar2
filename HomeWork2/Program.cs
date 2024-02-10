006a3ee67fa13a0d1956a29d798f33923cae8ed8
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-11 00:48:51 +0600

    Решение HomeWork 2

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 2: Напишите программу, которая принимает
+// на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠
+// 0 и выдаёт номер координатной четверти плоскости,
+// в которой находится эта точка.
+
+System.Console.Write("Введите X: ");
+int NumberX = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите Y: ");
+int NumberY = Convert.ToInt32(Console.ReadLine());
+
+if(NumberX > 0 && NumberY > 0)
+{
+System.Console.WriteLine("Точка в четверти № 1");
+}
+
+if(NumberX < 0 && NumberY > 0)
+{
+System.Console.WriteLine("Точка в четверти № 2");
+}
+
+if(NumberX < 0 && NumberY < 0)
+{
+System.Console.WriteLine("Точка в четверти № 3");
+}
+
+if(NumberX > 0 && NumberY < 0)
+{
+System.Console.WriteLine("Точка в четверти № 4");
+}
