a342295242da9610cdf986570b6316d5853a7ea6
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-10 23:26:43 +0600

    Решение Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Напишите программу, которая будет принимать на вход два
+// числа и выводить, является ли второе число кратным первому.
+// Если второе число некратно первому, то программа выводит
+// остаток от деления.
+// Примеры
+// 14, 5 => нет, 4
+// 16, 8 => да
+// 4, 3 => нет, 1
+
+System.Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber % secondNumber == 0)
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет, " + firstNumber % secondNumber);
+    // System.Console.WriteLine($"Нет, {firstNumber % secondNumber}");
+}
