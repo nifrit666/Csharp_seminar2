16aabbdf419065b14ce9ef8c06042f89b88c2831
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-10 23:52:22 +0600

    Решение HomeWork1

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+System.Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if(Number % 7 == 0 && Number % 23 == 0)
+{
+    System.Console.WriteLine("Введёное число кратно 7 и 23");
+}
+else
+{
+    System.Console.WriteLine($"Введённое число не кратно 7 и 23");
+}
