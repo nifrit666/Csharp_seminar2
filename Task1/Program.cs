e7242e9a81251965a27b1f7433eba68e1d823aed
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-10 22:59:30 +0600

    Решение Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+System.Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int leftDigit = number / 100;
+    int rightdigit = number % 10;
+    int newNumber = leftDigit * 10 + rightdigit;
+    System.Console.WriteLine(newNumber);
+}
+else
+{
+    System.Console.WriteLine("Введено не трёхзначное число!");
+}
