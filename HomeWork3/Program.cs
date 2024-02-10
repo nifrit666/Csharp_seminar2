20caff5c5a64ca0eeea8640481b1b9977bcaf6dc
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-11 02:32:12 +0600

    Решение HomeWork 3

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите число двухзначное число (от 10 до 99): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 9 && number < 100)
+{
+int firstNumber = number / 10;
+int lastNumber = number % 10;
+
+if(firstNumber > lastNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(lastNumber);
+}
+	}	
+else
+{
+    System.Console.WriteLine("Вы ввели не двухзначное число!");
+	}
