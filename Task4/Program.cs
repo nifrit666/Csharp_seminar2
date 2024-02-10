28f141d407dd7fde04c6b5c458a5fad6f2d47078
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-10 23:33:59 +0600

    Решение Task4 (кстати есть ошибка при вводе большого числа например 85456215165431)

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Напишите программу, которая выводит третью с конца
+// цифру заданного числа или сообщает, что третьей цифры
+// нет.
+// 456 => 6
+// 7812 => 1
+// 91 => Третьей цифры нет
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{
+    while(number > 999)
+    {
+        number /= 10; // number = number / 10
+    }
+    System.Console.WriteLine(number % 10);
+
+}
+else
+{
+    System.Console.WriteLine("В числе отсутствует третья цифра!");
+}
