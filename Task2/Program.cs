800f69b56338beb4f2083034836c6b5aa69e5fb7
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-10 23:21:43 +0600

    Решение Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+//Напишите программу, которая принимает на вход трёхзначное
+//число и возводит вторую цифру этого числа в степень, равную
+//третьей цифре.
+//Примеры
+//487 => 8^7 = 2 097 152
+//254 => 5^4 = 625
+//617 => 1
+
+System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number > 99 && number < 1000)
+{
+  int midldigit = number % 100 /10;
+  int ritdigit = number % 10;
+  int i = 0;
+  int result = 1;
+  while (i < ritdigit)
+  {
+    result = result * midldigit;
+    i ++;
+
+  }
+  System.Console.WriteLine(result);
+}
+else 
+{
+    System.Console.WriteLine("Ввели не трехзначное число");
+}
