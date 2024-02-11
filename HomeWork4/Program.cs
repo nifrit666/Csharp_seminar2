fd01c30ddfbe1e6eb877cb33618573129e0191d3
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-11 23:35:33 +0600

    Решение HomeWork 4 (Эталонное решение с обратным порядком)

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая на вход
+// принимает натуральное число N, а на выходе
+// показывает его цифры через запятую.
+
+
+// Задача 4: Вывод цифр числа через запятую
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите натуральное число N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        if (N < 10)
+        {
+            Console.WriteLine(N);
+        }
+        else
+        {
+            while (N > 0)
+            {
+                int currentDigit = N % 10;
+                N /= 10;
+                if (N > 0)
+                {
+                    Console.Write(currentDigit + ", ");
+                }
+                else
+                {
+                    Console.WriteLine(currentDigit);
+                }
+            }
+        }
+    }
+}
